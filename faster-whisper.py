209253b5f1f66ce8c4a297b1ba175037416946e1
illiashkurenko <ilya-shkurenko@mail.ru>
2024-09-27 14:46:46 +0300

    update model to v2

--- faster-whisper.py
+++ faster-whisper.py
@@ -1,7 +1,7 @@
 import time
 from faster_whisper import WhisperModel
 
-model_size = "large-v3"
+model_size = "large-v2"
 
 # Run on GPU with FP16
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
